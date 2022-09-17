2688bcfcff7854db269fd34a5a1d22dc341f32ac
Saurabh-Jadhav-1999 <saurabh.jadhav@bluepineapple.io>
2022-09-17 15:22:18 +0530

    Deployment Update 1

--- src/pages/HotelsListPage.jsx
+++ src/pages/HotelsListPage.jsx
@@ -1,6 +1,6 @@
 import Breadcrumb from "../components/breadcrumb/Breadcrumb"
 import { HotelSearchList } from "../components/hotelSearchList/HotelSearchList"
-import { SearchBar } from "../components/searchBar/SearchBar"
+import { SearchBar } from "../components/searchBar/SearchBar.jsx"
 import { Link } from "react-router-dom"
 
 export const HotelsListPage = () => {
